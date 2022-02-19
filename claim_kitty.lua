5d07a276d908a3c738176024808cdcc9456d0e97
Ordway54 <47861779+Ordway54@users.noreply.github.com>
2022-02-19 11:28:48 -0500

    Update claim_kitty.lua

--- claim_kitty.lua
+++ claim_kitty.lua
@@ -1,12 +1,25 @@
 kitty_zone_guid = Global.getVar("KITTY_ZONE_GUID")
 
--- when Claim Kitty button is pressed, deal kitty cards to player who pressed it, and announce it to all players
+
 function claimKitty(player)
-  local kitty_zone = getObjectFromGUID(kitty_zone_guid)
-  broadcastToAll(player.steam_name.." ("..player.color..")".. " has claimed the kitty.", {r=1,b=0,g=0})
-  for _, obj in ipairs(kitty_zone.getObjects()) do
-    if obj.tag == "Deck" then
-      obj.deal(3,player.color)
+  --[[
+  Moves the cards in the communal "kitty" to the hand of the player who calls this function
+  ]]--
+  hand_in_progress = Global.getVar("HAND_IN_PROGRESS")
+  kitty_claimed = Global.getVar("KITTY_CLAIMED")
+  if hand_in_progress == true and kitty_claimed == false then
+    local kitty_zone = getObjectFromGUID(kitty_zone_guid)
+    broadcastToAll(player.steam_name.." ("..player.color..")".. " has claimed the kitty.")
+    for _, obj in ipairs(kitty_zone.getObjects()) do
+      if obj.tag == "Deck" then
+        obj.deal(3,player.color)
+      end
     end
+    Global.setVar("KITTY_CLAIMED", true)
+
+  elseif hand_in_progress == false then
+    broadcastToColor("You can't claim the kitty if there's no hand in progress.", player.color)
+  else
+    broadcastToColor("The kitty has already been claimed.", player.color)
   end
 end
